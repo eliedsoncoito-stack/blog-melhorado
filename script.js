a5411e4b80b1908d789e69e537296bbb5de583d0
eliedsoncoito-stack <eliedson.coito@escola.pr.gov.br>
2026-08-07 20:06:22 -0300

    Fix button disable logic in counting functions

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 function contarCoracao(botao) {
+    if (botao.disabled) return;
+
     let numero = botao.querySelector("span");
     numero.innerText = Number(numero.innerText) + 1;
+    botao.disabled = true;
 }
 
 function contarCurtida(botao) {
+    if (botao.disabled) return;
+
     let numero = botao.querySelector("span");
     numero.innerText = Number(numero.innerText) + 1;
+    botao.disabled = true;
 }
